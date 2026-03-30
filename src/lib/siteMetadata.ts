ce476559e03bf41393ed28731bda7355a3833b15
suwon.ryu <suwon.ryu@gmail.com>
2026-03-30 21:32:59 +0900

    fix: use png for og image

--- src/lib/siteMetadata.ts
+++ src/lib/siteMetadata.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = '오늘의 카카오뱅크';
 export const SITE_DESCRIPTION = '오늘의 카카오뱅크 기사 요약 서비스';
-export const DEFAULT_OG_IMAGE_PATH = '/og-kabang-summary.svg';
+export const DEFAULT_OG_IMAGE_PATH = '/og-kabang-summary.png';
 
 export const DEFAULT_OG_IMAGE = {
   url: DEFAULT_OG_IMAGE_PATH,
